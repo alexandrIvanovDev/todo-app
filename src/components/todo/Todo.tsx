45a8c486c37897d1f1ee5f100533e2dff8b53a06
AlexandrIvanov <alexandr.1996@list.ru>
2023-09-18 14:52:06 +0300

    Add save to ls

--- src/components/todo/Todo.tsx
+++ src/components/todo/Todo.tsx
@@ -2,11 +2,22 @@ import Header from '../header/Header.tsx';
 import {TextField} from '../textField/TextField.tsx';
 import {List} from '../list/List.tsx';
 import cl from './Todo.module.scss'
-import {FC} from 'react';
+import {FC, useEffect} from 'react';
 import {useAppSelector} from '../../store/store.ts';
+import {LOCAL_STORAGE_TASKS_KEY, setReorderTasks} from '../../store/reducers/todo.ts';
+import {useDispatch} from 'react-redux';
 
 export const Todo: FC = () => {
     const tasks = useAppSelector(state => state.todo.tasks)
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        const res = localStorage.getItem(LOCAL_STORAGE_TASKS_KEY)
+        if (res) {
+            dispatch(setReorderTasks(JSON.parse(res)))
+        }
+    }, []);
 
     return (
         <div className={cl.container}>
--- src/store/reducers/todo.ts
+++ src/store/reducers/todo.ts
@@ -2,27 +2,14 @@ import {createSlice} from '@reduxjs/toolkit'
 import type {PayloadAction} from '@reduxjs/toolkit'
 import {v4} from 'uuid';
 
-export type TaskType = {
-    id: string
-    text: string
-    checked: boolean
-    order: number
-}
-
-export type FilterType = 'all' | 'active' | 'completed'
-
-type InitialState = {
-    tasks: TaskType[]
-    filter: FilterType
-    error: string | null
-}
+export const LOCAL_STORAGE_TASKS_KEY = 'tasks'
 
 const initialState: InitialState = {
     tasks: [
-        {id: v4(), text: 'Complete online JS course', checked: true, order: 1},
-        {id: v4(), text: '10 minutes meditation', checked: false, order: 2},
-        {id: v4(), text: 'Read for 1 hour', checked: false, order: 3},
-        {id: v4(), text: 'Complete Todo app', checked: false, order: 4}
+        // {id: v4(), text: 'Complete online JS course', checked: true},
+        // {id: v4(), text: '10 minutes meditation', checked: false},
+        // {id: v4(), text: 'Read for 1 hour', checked: false},
+        // {id: v4(), text: 'Complete Todo app', checked: false}
     ],
     filter: 'all',
     error: null
@@ -33,17 +20,18 @@ export const slice = createSlice({
     initialState,
     reducers: {
         addTask: (state, action: PayloadAction<string>) => {
-            const task: TaskType = {id: v4(), text: action.payload, checked: false, order: state.tasks.length + 1}
+            const task: TaskType = {id: v4(), text: action.payload, checked: false}
             if (action.payload.trim() === '') {
                 state.error = 'error'
                 return
             }
             state.tasks.push(task)
+            localStorage.setItem(LOCAL_STORAGE_TASKS_KEY, JSON.stringify(state.tasks))
         },
         deleteTask: (state, action: PayloadAction<string>) => {
             state.tasks = state.tasks.filter(task => task.id !== action.payload)
         },
-        changeTaskStatus: (state, action: PayloadAction<{id: string,  checked: boolean}>) => {
+        changeTaskStatus: (state, action: PayloadAction<{ id: string, checked: boolean }>) => {
             const task = state.tasks.find(t => t.id === action.payload.id)
             if (task) {
                 task.checked = action.payload.checked
@@ -55,15 +43,41 @@ export const slice = createSlice({
         changeFilter: (state, action: PayloadAction<FilterType>) => {
             state.filter = action.payload
         },
-        changeTaskText: (state, action: PayloadAction<{id: string, text: string}>) => {
+        changeTaskText: (state, action: PayloadAction<{ id: string, text: string }>) => {
             const task = state.tasks.find(t => t.id === action.payload.id)
-            if (task) {
+            if (task && action.payload.text.length < 10) {
                 task.text = action.payload.text
             }
+        },
+        setReorderTasks: (state, action: PayloadAction<Array<TaskType>>) => {
+            state.tasks = action.payload
         }
     }
 })
 
-export const {addTask, deleteTask, changeTaskStatus, clearCompletedTask, changeFilter, changeTaskText} = slice.actions
+export const {
+    addTask,
+    deleteTask,
+    changeTaskStatus,
+    clearCompletedTask,
+    changeFilter,
+    changeTaskText,
+    setReorderTasks
+} = slice.actions
 
 export default slice.reducer
+
+//types
+export type TaskType = {
+    id: string
+    text: string
+    checked: boolean
+}
+
+export type FilterType = 'all' | 'active' | 'completed'
+
+type InitialState = {
+    tasks: TaskType[]
+    filter: FilterType
+    error: string | null
+}
